eb6e923f21509be9708a28b3909cbd3acaa54582
Nikita Kuzmin <nikita.kuzmaaa@gmail.com>
2024-04-15 21:08:23 +0300

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { Metadata } from 'next'
+import { Jost } from 'next/font/google'
+
+import { AdminBar } from './_components/AdminBar'
+import { Providers } from './_providers'
+import { InitTheme } from './_providers/Theme/InitTheme'
+import { mergeOpenGraph } from './_utilities/mergeOpenGraph'
+
+import './_css/app.scss'
+
+const jost = Jost({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+  variable: '--font-jost',
+})
+
+export default async function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <head>
+        <InitTheme />
+        <link rel="icon" href="/favicon.ico" sizes="32x32" />
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+      </head>
+      <body className={jost.variable}>
+        <Providers>
+          <AdminBar />
+          <main className="main">{children}</main>
+        </Providers>
+      </body>
+    </html>
+  )
+}
+
+export const metadata: Metadata = {
+  metadataBase: new URL(process.env.NEXT_PUBLIC_SERVER_URL || 'https://payloadcms.com'),
+  twitter: {
+    card: 'summary_large_image',
+    creator: '@payloadcms',
+  },
+  openGraph: mergeOpenGraph(),
+}
